9ad88aec605362a2b3fc412ef727fa07f774e391
Zaguran <Dalibor.Svrcek@seznam.cz>
2026-01-11 23:06:21 +0100

    add db schema, .gitignore

--- db/db_setup/set_env_time_hetzner.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DB_HOST=128.140.108.240
-set DB_NAME=trading
-set DB_USER=trading_user
-set DB_PASS=Investor1966!
-set DB_PORT=5432
-echo === Environment variables set for TimescaleDB ===
